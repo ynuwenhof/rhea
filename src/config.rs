e0672e0c530279cdbbe487ad6d813199f3541a94
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-08-17 14:23:35 +0200

    Add server address field to config

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,24 @@
 use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use std::str::FromStr;
+use toml::de;
 
 #[derive(Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
-pub struct Config {}
+pub struct Config {
+    server: Server,
+}
+
+impl FromStr for Config {
+    type Err = de::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        toml::from_str(s)
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+struct Server {
+    addr: SocketAddr,
+}
