c90a63d0555427f5b7c94e996d3c4d80f65b1eea
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-08-17 16:52:54 +0200

    Add config load function with fallback value

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,26 @@
+use color_eyre::eyre::WrapErr;
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::path::Path;
 use std::str::FromStr;
+use tokio::fs;
 use toml::de;
 
-#[derive(Deserialize, Serialize)]
+#[derive(Default, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct Config {
-    server: Server,
+    #[serde(default)]
+    pub server: Server,
+}
+
+impl Config {
+    pub async fn load(path: impl AsRef<Path>) -> color_eyre::Result<Self> {
+        fs::read_to_string(path)
+            .await
+            .map(|s| Self::from_str(&s))
+            .unwrap_or_else(|_| Ok(Self::default()))
+            .wrap_err("Unable to parse config")
+    }
 }
 
 impl FromStr for Config {
@@ -19,6 +33,19 @@ impl FromStr for Config {
 
 #[derive(Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
-struct Server {
-    addr: SocketAddr,
+pub struct Server {
+    #[serde(default = "default_socket_addr")]
+    pub addr: SocketAddr,
+}
+
+impl Default for Server {
+    fn default() -> Self {
+        Self {
+            addr: default_socket_addr(),
+        }
+    }
+}
+
+fn default_socket_addr() -> SocketAddr {
+    SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use crate::config::Config;
+use color_eyre::eyre::eyre;
+
 mod config;
 
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let config_dir = dirs::config_dir()
+        .map(|p| p.join("rhea"))
+        .ok_or_else(|| eyre!("Unable to locate config directory path"))?;
+
+    let _config = Config::load(config_dir.join("rhea.toml")).await?;
+
+    Ok(())
 }
