9ee9f6774e50ba7344febd5eb3a7e5b3b652889c
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-09-07 09:14:37 +0200

    Add socks5 specific method settings to config

--- src/config.rs
+++ src/config.rs
@@ -17,7 +17,7 @@ impl Config {
     pub async fn load(path: impl AsRef<Path>) -> color_eyre::Result<Self> {
         fs::read_to_string(path)
             .await
-            .map(|s| Self::from_str(&s))
+            .map(|s| s.parse())
             .unwrap_or_else(|_| Ok(Self::default()))
             .wrap_err("Unable to parse config")
     }
@@ -36,16 +36,37 @@ impl FromStr for Config {
 pub struct Server {
     #[serde(default = "default_socket_addr")]
     pub addr: SocketAddr,
+    #[serde(default)]
+    pub socks: Socks,
 }
 
 impl Default for Server {
     fn default() -> Self {
         Self {
             addr: default_socket_addr(),
+            socks: Default::default(),
         }
     }
 }
 
 fn default_socket_addr() -> SocketAddr {
     SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080)
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct Socks {
+    #[serde(default)]
+    pub auth: bool,
+    #[serde(default)]
+    pub no_auth: bool,
+}
+
+impl Default for Socks {
+    fn default() -> Self {
+        Self {
+            auth: false,
+            no_auth: true,
+        }
+    }
 }
